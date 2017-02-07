3fe31483407e2316cdc9a218649d814fcd92ea6c
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-02-08 02:05:20 +0900

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add .travis.yaml

--- script/travis_test.sh
+++ script/travis_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ev
 
-if [ "${TEST_FOR_CLIENT}" = "true" ]; then
-    # Setup Nodejs to run client-side toolchain.
-    # Parhaps we should create and run Docker container which includes binary to test this repository.
-    nvm install 6
-    curl -o- -L https://yarnpkg.com/install.sh | bash # install yarn pkg
-    export PATH=$HOME/.yarn/bin:$PATH # add `yarn` to path.
+if [ "${TEST_FOR_SERVER}" = "true" ]; then
+    go build
+elif [ "${TEST_FOR_CLIENT}" = "true" ]; then
+    cd editors/monaco/
+    yarn install
+    yarn run lint
 else
     echo 'This code path is run unexpectedly' 1>&2
     exit 1
